2c6df51478f22d2710e4352f638dc087ad1ca22d
rafael1114 <37363448+rafael1114@users.noreply.github.com>
2018-05-02 01:11:40 -0500

    Add files via upload

---
+++ QueuesBLL/QueueBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RafaelMillis.QueuesDAL;
+using RafaelMillis.ViewModel;
+using RafaelMillis.QueueEntities;
+
+namespace RafaelMillis.BLL
+{
+    public class QueueBLL
+    {
+        QueueDAL dal = new QueueDAL();
+
+        public CustomerView GetActiveCustomer()
+        {
+            Customer cust = dal.GetActiveCustomer();
+            CustomerView custview = new CustomerView { CustomerName = cust.FirstName + " " + cust.LastName, QueueID = cust.QueueID, QueueTime = cust.Time.ToShortTimeString() };
+            return  custview;
+        }
+
+        public List<Customer> GetCustomerWaiting()
+        {
+            return dal.GetCustomerWaiting();
+        }
+
+        public void UpdateNextCustomer()
+        {
+            dal.UpdateNextCustomer();
+        }
+
+        public void InsertNewCustomer(Customer newCust)
+        {
+            dal.InsertNewCustomer(newCust);
+        }
+    }
+}
