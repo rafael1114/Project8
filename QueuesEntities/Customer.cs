f2a607399a62580d69c0c8868c3cee7695e47979
rafael1114 <37363448+rafael1114@users.noreply.github.com>
2018-05-02 01:12:00 -0500

    Add files via upload

---
+++ QueuesEntities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RafaelMillis.QueueEntities
+{
+    public class Customer
+    {
+        public int QueueID { get; set; }
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ QueuesEntities/CustomerView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RafaelMillis.ViewModel
+{
+    public class CustomerView
+    {
+        public int QueueID { get; set; }
+        public string CustomerName { get; set; }
+        public string QueueTime { get; set; }
+    }
+}
